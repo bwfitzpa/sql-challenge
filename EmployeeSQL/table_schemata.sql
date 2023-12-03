d63fef07875e8e05042756bf5358a6d5363b3e66
bwfitzpa <137216561+bwfitzpa@users.noreply.github.com>
2023-12-03 12:23:00 -0500

    final

--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -1,5 +1,4 @@
---Employees
-
+--Creating tables
 CREATE TABLE titles(
 	title_id VARCHAR(10) PRIMARY KEY,
 	title VARCHAR(50) NOT NULL
@@ -20,21 +19,10 @@ CREATE TABLE employees(
 	last_name VARCHAR(50) NOT NULL,
 	sex VARCHAR(5) NOT NULL,
 	hire_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES salaries(emp_no)
+	FOREIGN KEY (emp_no) REFERENCES salaries(emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 SELECT * FROM employees;
-
--------DATA ANALYSIS-------
---List the employee number, last name, first name, sex, and salary of each employee.
-SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
-FROM employees AS e
-JOIN salaries AS s
-	ON e.emp_no = s.emp_no;
-
---List the first name, last name, and hire date for the employees who were hired in 1986.
-SELECT hire_date AS hire_date, first_name, last_name
-FROM employees
-WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31' 
 
 CREATE TABLE dept_manager(
 	dept_no VARCHAR(20) NOT NULL,
@@ -45,19 +33,12 @@ CREATE TABLE dept_manager(
 );
 SELECT * FROM dept_manager;
 
---List the manager of each department along with their department number, 
-----department name, employee number, last name, and first name.
-SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
-FROM dept_manager as dm
-JOIN departments as d
-	on dm.dept_no = d.dept_no
-JOIN employees as e
-	on dm.emp_no = e.emp_no;
-
 CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(20) NOT NULL,
-	PRIMARY KEY(emp_no, dept_no)
+	PRIMARY KEY(emp_no, dept_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 SELECT * FROM dept_emp;
 
